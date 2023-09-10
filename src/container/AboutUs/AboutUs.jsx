4d73266b61782cf41b8e2a878e21b05df84b9a50
Rhys Pegrume <pegrume88@gmail.com>
2023-09-10 22:00:00 +0200

    added jhistory and knife image to about us and position everything and styled

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,18 +5,28 @@ import images from '../../constants/images';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
-    <div className='app__aboutus-overlay flex__center'>
-      <img src={images.G} alt='g letter' />
+   <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
     </div>
-    <div className='app__about-content flex__center'>
-      <div className='app__about-content_about'>
-        <h1 className='headtext__cormorant'>About Us</h1>
-        <img src={images.spoon} alt='about spoon'/>
-        <p className='p__opensans'>
-          Gericht, a culinary gem nestled in the heart of our vibrant city, invites you on a gastronomic journey like no other. 
-          Our passion for crafting exquisite dishes, using only the finest ingredients, is at the core of everything we do.</p>
-        <button type='button' className='custom__button'>Read More</button>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
   </div>
