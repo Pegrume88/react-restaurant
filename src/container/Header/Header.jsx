80b91ce885fc920bc2a74027ff736c1b5808a436
Rhys Pegrume <pegrume88@gmail.com>
2023-09-10 19:11:50 +0200

    added ccs and images to the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './Header.css';
+import images from '../../constants/images';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase The New Flavour'/>
+      <h1 className='app__header-h1'>The Key To Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}>The key to fine dining is like finding the secret garden of gastronomy, where each dish is a whimsical journey through flavors and textures. </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
 
