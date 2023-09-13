ad68bf4f7029f541a73f1e5a0256dc5a78f914c9
Rhys Pegrume <pegrume88@gmail.com>
2023-09-13 21:27:30 +0200

    added images and body to specials menu page.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -10,6 +10,32 @@ const SpecialMenu = () => (
       <SubHeading title="Menu That Fits Your Palatte" />
       <h1 className='headtext__cormorant'>Today's Special</h1>
     </div>
+    <div className='app__specialMenu-menu'>
+      <div className='app__specialMenu-menu_wine flex__center'>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.price} price={wine.price} tags={wine.tags}/>
+
+          ))}
+        </div>
+      </div>
+      <div className='app__specialMenu_menu_img'>
+        <img src={images.menu} alt='menu img'/>
+      </div>
+      <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.cocktails.map((cocktails, index) => (
+            <MenuItem key={cocktails.title + index} title={cocktails.price} price={cocktails.price} tags={cocktails.tags} />
+          ))}
+        </div>
+      </div>
+      
+    </div>
+    <div style={{marginTop:'15px'}}>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
   </div>
 );
 
