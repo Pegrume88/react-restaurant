53c6daf175c27f5cb7c91fbe16c3ce7b8f6072a2
Rhys Pegrume <pegrume88@gmail.com>
2023-09-14 21:26:39 +0200

    all content added and styled for special menu page as well as made responsive on phones tables and lg screens

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,7 +12,7 @@ const SpecialMenu = () => (
     </div>
     <div className='app__specialMenu-menu'>
       <div className='app__specialMenu-menu_wine flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
         <div className='app__specialMenu_menu_items'>
           {data.wines.map((wine, index) => (
             <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
@@ -20,11 +20,11 @@ const SpecialMenu = () => (
           ))}
         </div>
       </div>
-      <div className='app__specialMenu_menu_img'>
+      <div className='app__specialMenu-menu_img'>
         <img src={images.menu} alt='menu img'/>
       </div>
       <div className='app__specialMenu-menu_cocktails flex__center'>
-        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <p className='app__specialMenu-menu_heading'>Cocktails</p>
         <div className='app__specialMenu_menu_items'>
           {data.cocktails.map((cocktails, index) => (
             <MenuItem key={cocktails.title + index} title={cocktails.title} price={cocktails.price} tags={cocktails.tags} />
