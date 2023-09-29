8b95a9baa6093bdd187fcdcb9622c08aec689450
Rhys Pegrume <pegrume88@gmail.com>
2023-09-29 16:03:49 +0200

    images added to gallery and scroll function along all styles and media querys added to css

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -4,8 +4,19 @@ import {SubHeading} from '../../components'
 import {images} from '../../constants'
 import './Gallery.css';
 
-const Gallery = () => {
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
 
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    }else {
+      current.scrollLeft += 300;
+    }
+  }
   return (
     <div className='app__gallery flex__center'>
       <div className='app__gallery-content'>
@@ -15,6 +26,21 @@ const Gallery = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.
         </p>
         <button type='button' className='custom__button'>View More</button>
+      </div>
+
+      <div className='app__gallery-images'>
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='app__gallery-images_arrows'>
+        <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
+        <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
       </div>
     </div>
   );
