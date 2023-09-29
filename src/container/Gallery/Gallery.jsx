a0c658f9d2d18dc57f45e5c0dc765edd2b744a6a
Rhys Pegrume <pegrume88@gmail.com>
2023-09-29 12:13:10 +0200

    imported icons from reacr-icons

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import {SubHeading} from '../../components'
 import {images} from '../../constants'
 import './Gallery.css';
