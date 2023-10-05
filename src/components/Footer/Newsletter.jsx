c803ca816a44563ed8938865097d005ec42377d1
Rhys Pegrume <pegrume88@gmail.com>
2023-10-05 10:18:54 +0200

    added subheading and h1 +p elements to the newsletter

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -7,6 +7,8 @@ const Newsletter = () => (
   <div className='app__newsletter'>
     <div className='app__newsletter-heading'>
       <SubHeading title="Newsletter" />
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
     </div>
   </div>
 );
